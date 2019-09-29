c4ab32002504eab1c0ea3ba2726ae52465fa412d
Ashley <ajab1470@gmail.com>
2019-09-29 18:54:12 -0400

    Began time implementation
    Fixed more documentation and spacing. Created and tested a function to parse harvest times file
    
    TODO: Time functionality in harvests.py
          Switch command line implementation from sys.argv to argparse

--- file_reader.py
+++ file_reader.py
@@ -3,3 +3,31 @@ Author: Ashley Beckers
 File: file_reader.py
 Description: Reads and parses files related to merge dragons into data usable by program
 """
+
+def time_file_parser(filename):
+    """
+    Takes the name of a file with dragon harvest time information by species and puts it into a dictionary
+    :pre: file must be organized such that:
+            each line has one species' information
+            flower level and nod number are separated by :
+            entries are separated by a space
+            species name is separated from entries by a space
+    :param filename: name of the file
+    :return: a dictionary of the data | {species(string): {flower level(int): number of nods(int)}}
+    """
+    dragon_harvest_times = {}
+    with open(filename) as file:
+        duplicates = False # whether or not duplicates have been found yet, used to avoid multiple error statements
+        for line in file:
+            line = line.strip().split(' ')
+            species = line[0]
+            entries = {}
+            for entry in line:
+                entry = entry.split(':')
+                entries[entry[0]] = entry[1]
+            if species in dragon_harvest_times and not duplicates:
+                print('One or more lines marked duplicate and ignored')
+                duplicates = True
+            elif species not in dragon_harvest_times:
+                dragon_harvest_times[species] = entries
+    return dragon_harvest_times
--- harvests.py
+++ harvests.py
@@ -4,23 +4,27 @@ Author: Ashley Beckers
 Description: Simple Python program that will tell you how many harvests it will take to reach your goal in the game
  Merge Dragons. Eventually (if I can find the time to collect the data) it will tell you how long that will take and
  possibly allow you to perfect your strategy during events. Enjoy!
+
+Source of time information:
+It is found that a dragon will 'nod' a certain, consistent number of times in a harvest. This animation does take a set
+amount of time, 0.8 seconds. A nod is defined by a full cycle of down and up, ending when the dragon returns to start.
+Dragons typically harvest things in some amount of half nods, or close enough to it that rounding will not be that
+catastrophic.
+
+I have counted several of these times, which are unique to each species of dragon, and stored them in harvest_times.txt.
+This file is by no means complete and you are welcome to substitute your own (replace TIMES_FILE). Be sure to keep the
+format the same and put your times in terms of nods (or, if in seconds, change SECONDS_PER_NOD to 1)
 """
 import sys
 from pprint import PrettyPrinter
 import math
-
-"""
-TODO: Implement time using nod conversions
-It is found that a dragon will 'nod' a certain, consistent number of times in a harvest. This animation does take a set
-amount of time, 0.8 seconds. A nod is defined by a full cycle of down and up, ending when the dragon returns to start. 
-Dragons typically harvest things in some amount of half nods, or close enough to it that rounding will not be that 
-catastrophic
-"""
-
+import file_reader
 pprint = PrettyPrinter(width=1).pprint
+TIMES_FILE = "harvest_times.txt"
+SECONDS_PER_NOD = 0.8
 
 
-def amt_each_item(goal_lvl,current_lvl,goal_amt):
+def amt_each_item(goal_lvl, current_lvl, goal_amt):
     """
     Determines the amount of each orb obtained by only merging by fives
     :param goal_lvl: the level of the desired life orb
@@ -39,7 +43,8 @@ def amt_each_item(goal_lvl,current_lvl,goal_amt):
         amts[i] = goal_amt%2
     return amts
 
-def num_five_matches(goal_lvl,current_lvl,goal_amt):
+
+def num_five_matches(goal_lvl, current_lvl, goal_amt):
     """
     number of merges using only 5 merges to get the most profitable result
     :param goal_lvl: the level of the desired life orb
@@ -76,6 +81,7 @@ def main():
     """
     print("Note: Life essences are level 0, life orbs are level 1, etc., so a level 9 life orb is level 9 in this program.")
     print("      This program is only optimized for harvesting life orbs from life trees.\n")
+
     if len(sys.argv) == 1:
         goal_lvl = int(input("What level are you aiming for? "))
         goal_amt = int(input("How many of those do you want? "))
@@ -88,12 +94,15 @@ def main():
         goal_lvl = int(sys.argv[1])
         goal_amt = int(sys.argv[2])
         current_lvl = int(sys.argv[3])
+
     print("Harvest exactly " + str(num_five_and_three_matches(goal_lvl, current_lvl, goal_amt)) + " level " +
           str(current_lvl) + " things.")
     print("Or, to always merge five, harvest ", num_five_matches(goal_lvl, current_lvl, goal_amt), "level", current_lvl,
           "things.")
     print("If you are always merging by five, you will have (lvl:amt):")
     pprint(amt_each_item(goal_lvl, current_lvl, goal_amt))
+
+    harvest_times = file_reader.time_file_parser(TIMES_FILE)
 
 
 if __name__ == "__main__":
